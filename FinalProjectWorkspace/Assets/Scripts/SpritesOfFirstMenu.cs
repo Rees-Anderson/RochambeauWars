2efd7d43ceb8587dbbaf3db913cce49eef2999b2
Rees Anderson <rpa7@uw.edu>
2021-06-08 17:05:46 -0700

    Spiced Up Main Menu

--- FinalProjectWorkspace/Assets/Scripts/SpritesOfFirstMenu.cs
+++ FinalProjectWorkspace/Assets/Scripts/SpritesOfFirstMenu.cs
@@ -8,36 +8,22 @@ using UnityEngine;
  * Game Design Project
  */
 
-public class FieldManual : MonoBehaviour
+public class SpritesOfFirstMenu : MonoBehaviour
 {
     public MainMenuLogic MainMenuLogic;
 
     public GenericDisappearReappearScript[] thingsToMakeDissappear;
 
-    public Sprite[] manualPages;
-
-    public SpriteRenderer page;
-
-    public int currentPage;
-
-    private float r;
-    private float g;
-    private float b;
-    private float defaultAlpha;
-
     // Start is called before the first frame update
     void Start()
     {
-        r = GetComponent<Renderer>().material.color.r;
-        g = GetComponent<Renderer>().material.color.g;
-        b = GetComponent<Renderer>().material.color.b;
-        defaultAlpha = GetComponent<Renderer>().material.color.a;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (MainMenuLogic.state == "Field Manual")
+        if (MainMenuLogic.state == "Default")
         {
             reappear();
         }
@@ -45,13 +31,10 @@ public class FieldManual : MonoBehaviour
         {
             dissappear();
         }
-
-        page.sprite = manualPages[currentPage];
     }
 
     public void dissappear()
     {
-        GetComponent<Renderer>().material.color = new Color(r, g, b, 0);
         for (int i = 0; i < thingsToMakeDissappear.Length; i++)
         {
             thingsToMakeDissappear[i].dissappear();
@@ -60,7 +43,6 @@ public class FieldManual : MonoBehaviour
 
     public void reappear()
     {
-        GetComponent<Renderer>().material.color = new Color(r, g, b, defaultAlpha);
         for (int i = 0; i < thingsToMakeDissappear.Length; i++)
         {
             thingsToMakeDissappear[i].reappear();
